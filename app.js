ed681cc702de7b8b9f1b484a2f3c3613590cf3f7
coderaashna <aashna.iitk@gmail.com>
2017-09-21 22:36:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function updateColor() {
+    var redVal = document.getElementById('redVal').value
+    var blueVal = document.getElementById('blueVal').value;
+    var greenVal = document.getElementById('greenVal').value;
+    var colorArea = document.getElementById('color-area');
+    var newColorValue = 'rgb(' + redVal + ',' + greenVal + ',' + blueVal + ')';
+    colorArea.style.backgroundColor = newColorValue;
+
+}
