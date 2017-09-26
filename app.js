0a2c5369af524c9206ef353d1c01ac27e048cad7
coderaashna <aashna.iitk@gmail.com>
2017-09-26 22:55:35 +0530

    Formatted the code
    * *Set the default value of slider

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function updateColor() {
-    var redVal = document.getElementById('red-val').value
-    var blueVal = document.getElementById('blue-val').value;
-    var greenVal = document.getElementById('green-val').value;
+    var redVal = document.getElementById('redVal').value;
+    document.getElementById('redVal').value = 128;
+    var blueVal = document.getElementById('blueVal').value;
+    var greenVal = document.getElementById('greenVal').value;
     var colorArea = document.getElementById('color-area');
     var newColorValue = 'rgb(' + redVal + ',' + greenVal + ',' + blueVal + ')';
     colorArea.style.backgroundColor = newColorValue;
+
 }
